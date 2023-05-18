a8ddf40c62e8c26b0406432ae5fc98a69bf76c8a
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-05-18 16:20:43 -0600

    using for loops lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names)
+{
+    if (names == undefined || names == null) return null;
+    else
+    {
+        var marr = new Array();
+        for (var n = 0; n < names.length; n++)
+        {
+            marr.push("Thank you, " + names[n] + ", for the wonderful surprise gift!");
+        }
+        return marr;
+    }
+}
+
+function countDown(n)
+{
+    for (var x = n; (x > 0 || x == 0) && (x < n || x == n); x--)
+    {
+        console.log(x);
+    }
+}
